7bd92fbc67980648cc5d8622775a355b2972b5c4
3cl1pz3 <penaginalex@gmail.com>
2024-11-26 12:24:37 +0900

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class DifferenceCalculator
+{
+    public static int CalculateDifference(int n)
+    {
+        int absoluteDifference = Math.Abs(n - 123);
+
+        if (n > 123)
+        {
+            return absoluteDifference * 3;
+        }
+        else
+        {
+            return absoluteDifference;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int n))
+        {
+            int difference = CalculateDifference(n);
+            Console.WriteLine($"Абсолютная разность между {n} и 123: {difference}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+        }
+    }
+}
